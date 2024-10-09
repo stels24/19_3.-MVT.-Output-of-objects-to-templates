8b932758af0346ac96fe070350747332bd91a8b3
stels24 <166283589+stels24@users.noreply.github.com>
2024-10-09 22:49:13 +0300

    Update models.py

--- task1/models.py
+++ task1/models.py
@@ -14,7 +14,7 @@ class Game(models.Model):
     size = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='games')
+    #buyer = models.ManyToManyField(Buyer, related_name='games')
 
     def __str__(self):
         return self.title
