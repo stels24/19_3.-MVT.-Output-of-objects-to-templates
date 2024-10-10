b4f26e230838b08f7576b369ff83275abc8de4de
stels24 <166283589+stels24@users.noreply.github.com>
2024-10-10 21:46:48 +0300

    Update settings.py

--- UrbanDjango/settings.py
+++ UrbanDjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'UrbanDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'tamplates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
